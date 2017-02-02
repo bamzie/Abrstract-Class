643cebac0f1828423ac3a2b8296f4bf169ff4ca0
Peter Chudinov <peterchudinov@icloud.com>
2017-02-01 19:14:39 -0800

    templates

--- main.cpp
+++ main.cpp
@@ -1,14 +1,32 @@
 //
-//  main.cpp
-//  Abrstract Class
-//
-//  Created by Brian Morales on 2/1/17.
-//  Copyright © 2017 Brian Morales. All rights reserved.
+//  Peter Chudinov (pj6593), Brian Moralez (kp5575)
+//  Person : Employee : Templates
 //
 
 #include <iostream>
+#include <vector>
 #include "Person.h"
 #include "Employee.h"
+
+template<typename T>
+double totalHours(vector<Employee> e) {
+  double sum = 0;
+  for(int i = 0; i < e.size(); i++) {
+    sum += e.at(i).getHrs();
+  }
+  return sum;
+}
+
+template<typename T>
+double totalHours(vector<T> e) {
+  double sum = 0;
+  double avg = 0;
+  for(int i = 0; i < e.size(); i++) {
+    sum += e.at(i).getHrs();
+  }
+  avg = sum / e.size();
+  return avg;
+}
 
 int main()
 {
